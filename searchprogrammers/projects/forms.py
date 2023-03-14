5b2c1d5c97a4ed3f56bfa83aade437af19ece8dc
Kingsman <shadman.wasif@gmail.com>
2023-03-14 16:32:27 +0600

    Template installed

--- searchprogrammers/projects/forms.py
+++ searchprogrammers/projects/forms.py
@@ -1,9 +1,18 @@
 from django.forms import ModelForm
-
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         exclude = ['vote_total', 'vote_ratio']
-        fields = '__all__'
+        # fields = '__all__'
+        widgets = {
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class':'input', })
