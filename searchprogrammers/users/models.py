6824fd740883cb89acce838b56ce98cceea1baba
Kingsman <shadman.wasif@gmail.com>
2023-06-08 16:44:28 +0600

    User Registration and Login Functionality Added

--- searchprogrammers/users/models.py
+++ searchprogrammers/users/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
-
+from django.db.models.signals import post_save, post_delete
+# from django.dispatch import receiver
 
 # Create your models here.
 
@@ -20,10 +21,11 @@ class Profile(models.Model):
     social_youtube = models.CharField(max_length=200, null=True, blank=True)
     social_website = models.CharField(max_length=200, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
     
 class Skill(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
@@ -35,7 +37,4 @@ class Skill(models.Model):
     
     def __str__(self):
         return self.name
-    
-
-    
-    
+   
--- searchprogrammers/users/views.py
+++ searchprogrammers/users/views.py
@@ -1,17 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
 from .models import Profile
+from django.contrib import messages
+
+from .forms import CustomUserCreationForm
+
 # Create your views here.
+
+def loginUser(request):
+    page = 'login'
+    context = {'page': page}
+    if request.user.is_authenticated:
+        # user is already authenticated, no need to visit login page
+        return redirect('profiles')
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username doesnot exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'Username or Password is incorrect')
+
+    return render(request, 'users/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    messages.error(request, 'User was logged out successfully!')
+
+    return redirect('login')
+
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+    
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            
+            messages.success(request, 'User account was created Successfully!')
+
+            login(request, user)
+            return redirect('profiles')
+        
+        else:
+            messages.error(request, "An error occurred during registration")
+        
+    context = {'page':page, 'form':form}
+    return render(request, 'users/login_register.html', context)
+
 
 def profiles(request):
     profiles = Profile.objects.all()
     context = {'profiles': profiles}
     return render(request, 'users/profiles.html', context)
 
+
 def userProfile(request, pk):
     profile = Profile.objects.get(id=pk)
-    
+
     topSkills = profile.skill_set.exclude(description__exact="")
-    otherSkills = profile.skill_set.filter(description = "")
-    
-    context = {'profile':profile, 'topSkills': topSkills, 'otherSkills' : otherSkills}
+    otherSkills = profile.skill_set.filter(description= "")
+
+    context = {'profile': profile, 'topSkills': topSkills, 'otherSkills' : otherSkills}
     return render(request, 'users/user-profile.html', context)
