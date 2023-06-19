802cce1cb1288c0d9b9b9bc2c19ecc4617f79734
Kingsman <shadman.wasif@gmail.com>
2023-06-19 16:56:25 +0600

    Project Review Functionality Added

--- searchprogrammers/projects/views.py
+++ searchprogrammers/projects/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project, Tag
-from .forms import ProjectForm
+from .forms import ProjectForm, ReviewForm
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .utils import searchProjects, paginateProjects
+from django.contrib import messages
 
 
 # Create your views here.
@@ -18,7 +19,24 @@ def projects(request):
 
 def project(request,pk):
     projectObj = Project.objects.get(id=pk)
-    return render(request, "projects/single-project.html", {'project': projectObj})
+    form = ReviewForm()
+    
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = projectObj
+        review.owner = request.user.profile
+        review.save()
+        
+        projectObj.getVoteCount
+        
+        messages.success(request, 'You review was submitted succesfully!')
+        return redirect('project', pk=projectObj.id)
+        
+    projectObj.getVoteCount
+    
+    
+    return render(request, "projects/single-project.html", {'project': projectObj, 'form': form})
 
 @login_required(login_url="login")
 def createProject(request):
