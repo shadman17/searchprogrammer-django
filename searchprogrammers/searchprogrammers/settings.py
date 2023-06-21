aa3d55c2c7b47304f48eceb3333230a31424e824
Kingsman <shadman.wasif@gmail.com>
2023-06-21 16:27:14 +0600

    Minor Changes

--- searchprogrammers/searchprogrammers/settings.py
+++ searchprogrammers/searchprogrammers/settings.py
@@ -119,8 +119,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hazardousgaming10@gmail.com'
-EMAIL_HOST_PASSWORD = 'gtpumvddymmlysby'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 # Static files (CSS, JavaScript, Images)
